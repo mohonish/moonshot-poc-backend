47a861c9bb16b810342f22de1358741f3be02ba4
mohonish <mohonish.c@gmail.com>
2020-05-30 19:55:24 -0400

    movde to subrouter with api/v1/iss/coordinates

--- main.go
+++ main.go
@@ -9,25 +9,33 @@ import (
 	"net/http"
 )
 
+const contentTypeKey string = "Content-Type"
+const contentTypeVal string = "application/json"
+
 func main() {
+	// Set CLI Flags
 	setCLIFlags()
 
-	// Create mux router
+	// Create mux base router
 	r := mux.NewRouter()
-	r.HandleFunc("/", baseRouteGET).Methods(http.MethodGet)
-	r.HandleFunc("/", routeNotFound)
+
+	// Create mux subrouter
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/iss/coordinates", baseRouteGET).Methods(http.MethodGet)
+	api.HandleFunc("", routeNotFound)
+
 	// Listen on 8081
 	log.Fatal(http.ListenAndServe(":8081", r))
 }
 
 func baseRouteGET(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentTypeKey, contentTypeVal)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "get called"}`))
 }
 
 func routeNotFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(contentTypeKey, contentTypeVal)
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte(`{"message": "not found"}`))
 }
