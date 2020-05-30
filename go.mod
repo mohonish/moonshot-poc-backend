d63103de24695a00455ab9da1a6b9fdeeff93485
mohonish <mohonish.c@gmail.com>
2020-05-30 19:46:30 -0400

    forgot to add files to prev commit. created basic rest api with mux router

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mohonish/moonshot-backend
 
 go 1.14
 
-require github.com/spf13/viper v1.3.2
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/spf13/viper v1.3.2
+)
--- main.go
+++ main.go
@@ -3,24 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/mohonish/moonshot-backend/version"
-	
+	"log"
+	"net/http"
 )
 
 func main() {
+	setCLIFlags()
 
-    
+	// Create mux router
+	r := mux.NewRouter()
+	r.HandleFunc("/", baseRouteGET).Methods(http.MethodGet)
+	r.HandleFunc("/", routeNotFound)
+	// Listen on 8081
+	log.Fatal(http.ListenAndServe(":8081", r))
+}
+
+func baseRouteGET(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "get called"}`))
+}
+
+func routeNotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message": "not found"}`))
+}
+
+func setCLIFlags() {
 	versionFlag := flag.Bool("version", false, "Version")
 	flag.Parse()
 
 	if *versionFlag {
 		fmt.Println("Build Date:", version.BuildDate)
-        fmt.Println("Git Commit:", version.GitCommit)
-        fmt.Println("Version:", version.Version)
-        fmt.Println("Go Version:", version.GoVersion)
-        fmt.Println("OS / Arch:", version.OsArch)
+		fmt.Println("Git Commit:", version.GitCommit)
+		fmt.Println("Version:", version.Version)
+		fmt.Println("Go Version:", version.GoVersion)
+		fmt.Println("OS / Arch:", version.OsArch)
 		return
 	}
-	fmt.Println("Hello.")
-    
 }
